3f2a206d5742a477188e2e31347a71adf35b229a
Michael Loukeris <michael.loukeris@gmail.com>
2021-11-04 12:47:42 +0200

    Adding CustomUser model

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,20 @@
-# from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import ASCIIUsernameValidator
+from django.utils.translation import gettext_lazy as _
+from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    username_validator = ASCIIUsernameValidator()
+    email = models.EmailField(
+        _("email address"),
+        unique=True,
+        error_messages={
+            "unique": _("A user with that email address already exists."),
+        },
+    )
+
+    def clean(self):
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
+        self.username = self.username.lower()
--- config/settings/base.py
+++ config/settings/base.py
@@ -85,3 +85,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "accounts.CustomUser"
