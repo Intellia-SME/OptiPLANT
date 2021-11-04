bd20fe0b44dd0756baacff6e04d372e080fbb13a
Michael Loukeris <michael.loukeris@gmail.com>
2021-11-04 12:07:11 +0200

    Making the connection with PostgreSQLv14.0 in the dev env.

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,4 +1,16 @@
 from .base import *
 
 DEBUG = True
+
 ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': get_env_variable('DB_HOST'),
+        'NAME': get_env_variable('DB_NAME'),
+        'USER': get_env_variable('DB_USER'),
+        'PASSWORD': get_env_variable('DB_PASSWORD'),
+        'PORT': get_env_variable('DB_PORT'),
+    }
+}
