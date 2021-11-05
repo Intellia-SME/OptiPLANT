c8bcb17b1b8e535a822d8a380a6d1b93821a5497
Michael Loukeris <michael.loukeris@gmail.com>
2021-11-05 13:45:10 +0200

    Testingr CustomUserManager class

---
+++ apps/accounts/tests/test_managers.py
@@ -0,0 +1,13 @@
+import os
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+UserModel = get_user_model()
+
+
+class CustomUserManagerTests(TestCase):
+    def test_get_by_natural_key_returns_case_insensitive(self):
+        user = UserModel.objects.create(
+            username="guest", email="guest@guest.gr", password=os.environ['TEST_USER_PASS']
+        )
+        self.assertIsNotNone(UserModel._default_manager.get_by_natural_key(user.username.upper()))
