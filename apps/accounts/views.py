657b3ba426b9244bb0a37dbb66ed6e27d75f3d4f
Michael Loukeris <michael.loukeris@gmail.com>
2021-11-08 16:51:48 +0200

    Adding an initial login.html template

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -3,4 +3,6 @@ from django.contrib.auth import views as auth_views
 
 class LoginView(auth_views.LoginView):
     http_method_names = ['get', 'post']
+    # TODO: After home page creation
+    # redirect_authenticated_user = True
     template_name = 'accounts/login.html'
--- config/settings/base.py
+++ config/settings/base.py
@@ -87,3 +87,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "accounts.CustomUser"
+
+LOGIN_REDIRECT_URL = 'accounts:login'  # TODO: change to homepage
+
+LOGIN_URL = 'accounts:login'
