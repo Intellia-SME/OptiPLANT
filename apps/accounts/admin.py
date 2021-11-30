fb7fea508e86f31d880c311626ef7d9fc677d740
Michael Loukeris <michael.loukeris@gmail.com>
2021-11-30 12:30:16 +0200

    Creating a custom Signup form for registering new users on the admin page

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
+from .forms import SignupForm
 from .models import CustomUser
 
-admin.site.register(CustomUser, UserAdmin)
+
+class CustomUserAdmin(UserAdmin):
+    add_form = SignupForm
+    add_fieldsets = (
+        (
+            None,
+            {
+                'fields': (
+                    'username',
+                    'email',
+                    'password1',
+                    'password2',
+                ),
+            },
+        ),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
