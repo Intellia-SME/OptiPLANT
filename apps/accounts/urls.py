e82608b1970a38a04de973ce66fc4063510e2bfb
Michael Loukeris <michael.loukeris@gmail.com>
2021-11-30 13:31:05 +0200

    Adding singupview for creating new user

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import LoginView, LogoutView, ProfileView
+from .views import LoginView, LogoutView, ProfileView, SignupView
 
 app_name = 'accounts'
 
 
 urlpatterns = [
+    path('signup/', SignupView.as_view(), name='signup'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('login/', LoginView.as_view(), name='login'),
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,8 +1,33 @@
 from django.conf import settings
+from django.contrib.auth import authenticate, login
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseRedirect
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import FormView, TemplateView
+
+from .forms import SignupForm
+
+
+class SignupView(FormView):
+    http_method_names = ['get', 'post']
+    form_class = SignupForm
+    redirect_authenticated_user = True
+    template_name = 'accounts/signup.html'
+    success_url = reverse_lazy('accounts:profile')
+
+    def form_valid(self, form):
+        form.save()
+        username = form.cleaned_data.get('username')
+        raw_password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=raw_password)
+        login(self.request, user)
+        return super().form_valid(form)
+
+    def dispatch(self, request, *args, **kwargs):
+        if self.redirect_authenticated_user and self.request.user.is_authenticated:
+            return HttpResponseRedirect(self.get_success_url())
+        return super().dispatch(request, *args, **kwargs)
 
 
 class ProfileView(LoginRequiredMixin, TemplateView):
