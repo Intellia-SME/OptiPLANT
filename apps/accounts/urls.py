74395f9aab662c460450d7cf3dd9c7fd8c25675b
Michael Loukeris <michael.loukeris@gmail.com>
2021-12-07 19:20:30 +0200

    Adding functionality for password reset

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 
-from .views import LoginView, LogoutView, ProfileView
+from .views import (
+    LoginView,
+    LogoutView,
+    PasswordResetCompleteView,
+    PasswordResetConfirmView,
+    PasswordResetDoneView,
+    PasswordResetView,
+    ProfileView,
+)
 
 app_name = 'accounts'
 
@@ -9,4 +17,8 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('login/', LoginView.as_view(), name='login'),
+    path('password-reset/', PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset-done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseRedirect
+from django.shortcuts import resolve_url
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 
 
@@ -21,3 +23,63 @@ class LogoutView(auth_views.LogoutView):
     def post(self, request, *args, **kwargs):
         self.next = settings.CUSTOM_LOGOUT_REDIRECT_URL
         return HttpResponseRedirect(self.next)
+
+
+class PasswordResetView(auth_views.PasswordResetView):
+    http_method_names = ['get', 'post']
+    redirect_authenticated_user = True
+    subject_template_name = 'accounts/password_reset_email/password_reset_subject.txt'
+    email_template_name = 'accounts/password_reset_email/password_reset_body.html'
+    success_url = reverse_lazy('accounts:password_reset_done')
+    template_name = 'accounts/password_reset.html'
+
+    def get_default_redirect_url(self):
+        return resolve_url(settings.LOGIN_REDIRECT_URL)
+
+    def dispatch(self, request, *args, **kwargs):
+        if self.redirect_authenticated_user and self.request.user.is_authenticated:
+            return HttpResponseRedirect(self.get_default_redirect_url())
+        return super().dispatch(request, *args, **kwargs)
+
+
+class PasswordResetDoneView(auth_views.PasswordResetDoneView):
+    http_method_names = ['get']
+    redirect_authenticated_user = True
+    template_name = 'accounts/password_reset_done.html'
+
+    def get_default_redirect_url(self):
+        return resolve_url(settings.LOGIN_REDIRECT_URL)
+
+    def dispatch(self, request, *args, **kwargs):
+        if self.redirect_authenticated_user and self.request.user.is_authenticated:
+            return HttpResponseRedirect(self.get_default_redirect_url())
+        return super().dispatch(request, *args, **kwargs)
+
+
+class PasswordResetConfirmView(auth_views.PasswordResetConfirmView):
+    http_method_names = ['get', 'post']
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('accounts:password_reset_complete')
+    template_name = 'accounts/password_reset_confirm.html'
+
+    def get_default_redirect_url(self):
+        return resolve_url(settings.LOGIN_REDIRECT_URL)
+
+    def dispatch(self, request, *args, **kwargs):
+        if self.redirect_authenticated_user and self.request.user.is_authenticated:
+            return HttpResponseRedirect(self.get_default_redirect_url())
+        return super().dispatch(request, *args, **kwargs)
+
+
+class PasswordResetCompleteView(auth_views.PasswordResetCompleteView):
+    http_method_names = ['get']
+    redirect_authenticated_user = True
+    template_name = 'accounts/password_reset_complete.html'
+
+    def get_default_redirect_url(self):
+        return resolve_url(settings.LOGIN_REDIRECT_URL)
+
+    def dispatch(self, request, *args, **kwargs):
+        if self.redirect_authenticated_user and self.request.user.is_authenticated:
+            return HttpResponseRedirect(self.get_default_redirect_url())
+        return super().dispatch(request, *args, **kwargs)
