bbd786111987283b8ef3e3c4cb00790588a240a6
Michael Loukeris <michael.loukeris@gmail.com>
2021-12-17 15:53:48 +0200

    Adding a dummy home page view  in urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
+
+
+class HomeView(TemplateView):
+    def get_template_names(self):
+        if self.request.user.is_authenticated:
+            return 'home.html'
+        return 'guest.html'
+
 
 urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
     path('admin/', admin.site.urls),
     path('accounts/', include('apps.accounts.urls', namespace='accounts')),
 ]
