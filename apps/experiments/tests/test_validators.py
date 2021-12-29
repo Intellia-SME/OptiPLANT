808642b5a117e7c16730ff16a931074e27f391ef
Michael Loukeris <michael.loukeris@gmail.com>
2021-12-29 14:17:26 +0200

    Adding tests for Experiment validators->validate_size

---
+++ apps/experiments/tests/test_validators.py
@@ -0,0 +1,22 @@
+from django.core.exceptions import ValidationError
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import TestCase
+
+from ..validators import validate_size
+
+
+class ExperimentValidatorTest(TestCase):
+    def test_validation_error_with_big_file_more_than_1MB(self):
+        big_file = SimpleUploadedFile("demo_file.csv", b"Dummy")
+        big_file.size = 1048577
+        with self.assertRaises(ValidationError) as e:
+            validate_size(big_file)
+        self.assertEqual(e.exception.messages[0], 'demo_file.csv must be less than 1MB')
+
+    def test_no_validation_error_with_file_less_than_1MB(self):
+        big_file = SimpleUploadedFile("demo_file.csv", b"Dummy")
+        big_file.size = 1048576
+        try:
+            validate_size(big_file)
+        except ValidationError:
+            self.fail("big_file() raised ValidationError unexpectedly!")
